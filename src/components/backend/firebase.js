94008ed8c3dabe2c98fc0a214846ad781be3e29e
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-04-18 19:50:50 +0530

    Update firebase.js

--- src/components/backend/firebase.js
+++ src/components/backend/firebase.js
@@ -6,13 +6,7 @@ import "firebase/compat/storage";
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq7gJ8JuOCss6ol8ifw9uOBrfsXGUoO6E",
-  authDomain: "whatsappadvanced-d350b.firebaseapp.com",
-  projectId: "whatsappadvanced-d350b",
-  storageBucket: "whatsappadvanced-d350b.appspot.com",
-  messagingSenderId: "42957807468",
-  appId: "1:42957807468:web:b688132f4d934cc60a1d49",
-  measurementId: "G-T9PR8V62WR"
+ 
 };
 
 
