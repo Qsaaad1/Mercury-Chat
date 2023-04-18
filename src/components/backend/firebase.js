1bd05fba683177c595dbbb1a0240532228b3e31e
Harsh Mishra <hkmmishra18@gmail.com>
2023-04-18 20:36:45 +0530

    added .env , api keys hidden

--- src/components/backend/firebase.js
+++ src/components/backend/firebase.js
@@ -6,13 +6,13 @@ import "firebase/compat/storage";
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq7gJ8JuOCss6ol8ifw9uOBrfsXGUoO6E",
-  authDomain: "whatsappadvanced-d350b.firebaseapp.com",
-  projectId: "whatsappadvanced-d350b",
-  storageBucket: "whatsappadvanced-d350b.appspot.com",
-  messagingSenderId: "42957807468",
-  appId: "1:42957807468:web:b688132f4d934cc60a1d49",
-  measurementId: "G-T9PR8V62WR"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain:  `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket:  `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId:  `${process.env.REACT_APP_MESSAGE_SENDER_ID}`,
+  appId:  `${process.env.REACT_APP_APP_ID}`,
+  measurementId:  `${process.env.REACT_APP_MEASUREMENT_ID}`,
 };
 
 
